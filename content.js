28129431d678129e69fcb3ecdf4542610a673281
NasirAziz-Adept <121924718+NasirAziz-Adept@users.noreply.github.com>
2024-05-08 12:06:55 +0500

    changes to new requirements

--- content.js
+++ content.js
@@ -1,144 +1,129 @@
-let speed = 750;
-let shouldContinueSwiping = true;
-let intervalId;
+let shouldContinueSwiping = true
+let intervalId
+let pauseIntervalId
+let result = await chrome.storage.local.get(['speed'])
+let speedInSeconds = result.speed || 1 // Default to 750 ms if not set // Base swipe speed in seconds
 
-// Main function to check profile and swipe
+function simulateHumanBehavior() {
+	const randomSpacePresses = Math.floor(Math.random() * 3) + 1 // Generates 1, 2, or 3 space presses
+	for (let i = 0; i < randomSpacePresses; i++) {
+		setTimeout(() => {
+			document.body.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 32 })) // 32 is the keyCode for space
+			document.body.dispatchEvent(new KeyboardEvent('keyup', { keyCode: 32 }))
+		}, i * 200) // Space out the key presses by 200 ms each
+	}
+}
+
 function checkProfileAndSwipe() {
 	if (shouldContinueSwiping) {
-		const viewProfileButton = document.getElementsByClassName(
-			'P(0) Trsdu($normal) Sq(28px) Bdrs(50%) Cur(p) Ta(c) Scale(1.2):h CenterAlign M(a) focus-button-style'
-		)[0];
+		simulateHumanBehavior()
 
-		if (viewProfileButton) viewProfileButton.click();
+		// Randomize the time before next swipe
+		const timeToNextSwipe = (speedInSeconds + (Math.floor(Math.random() * 3) - 1)) * 1000
 
 		setTimeout(() => {
-			const languagesDiv = document.getElementsByClassName(
-				'Bgc($c-ds-background-primary) Fxg(1) Z(1) Pb(100px)'
-			);
+			// Perform the swipe logic here
+			performSwipeLogic()
+		}, timeToNextSwipe)
+	}
+}
 
-			if (languagesDiv && languagesDiv.length > 0) {
-				// Obtain profile information
-				const nameInfo = document.getElementsByClassName(
-					'Typs(display-1-strong) Fxs(1) Fxw(w) Pend(8px) M(0) D(i)'
-				)[0].innerHTML;
+function performSwipeLogic() {
+	const languagesDiv = document.getElementsByClassName(
+		'Bgc($c-ds-background-primary) Fxg(1) Z(1) Pb(100px)'
+	)
 
-				let shouldSwipeRight = false;
+	if (languagesDiv && languagesDiv.length > 0) {
+		const nameInfo = document.querySelector(
+			'.Typs(display-1-strong) Fxs(1) Fxw(w) Pend(8px) M(0) D(i)'
+		)
 
-				// Check if language contains Russian or Ukrainian
-				[...languagesDiv]
-					.filter((a) => a.textContent.includes('Languages I Know'))
-					.forEach((a) => {
-						if (!shouldSwipeRight)
-							shouldSwipeRight = a.textContent.includes('Russian') || a.textContent.includes('Ukrainian');
-					});
-
-				// Check if name is in Cyrillic script
-				if (!shouldSwipeRight && nameInfo) {
-					// Regex to check for Cyrillic script
-					const cyrillicPattern = /[\u0400-\u04FF]/;
-					if (cyrillicPattern.test(nameInfo.textContent)) {
-						shouldSwipeRight = true;
-					}
+		let shouldSwipeRight = false
+		;[...languagesDiv]
+			.filter((a) => a.textContent.includes('Languages I Know'))
+			.forEach((a) => {
+				if (!shouldSwipeRight) {
+					shouldSwipeRight =
+						a.textContent.includes('Russian') || a.textContent.includes('Ukrainian')
 				}
+			})
 
-				// Swipe right or left based on the conditions
-				if (shouldSwipeRight) {
-					swipeRight();
-				} else {
-					swipeLeft();
-				}
+		if (!shouldSwipeRight && nameInfo) {
+			const cyrillicPattern = /[\u0400-\u04FF]/
+			if (cyrillicPattern.test(nameInfo.textContent)) {
+				shouldSwipeRight = true
 			}
-		}, speed / 1.5);
+		}
 
-		// const observer = new MutationObserver(function (mutations, mutationInstance) {
-		// 	const languagesDiv = document.getElementsByClassName(
-		// 		'Bgc($c-ds-background-primary) Fxg(1) Z(1) Pb(100px)'
-		// 	);
-		// 	if (languagesDiv && languagesDiv.length > 0) {
-		// 		// Obtain profile information
-		// 		const languageInfo = languagesDiv[0].querySelector('div:nth-child(5) > div > div'); // Change '.language-class' to the actual class
-		// 		const nameInfo = document.getElementsByClassName(
-		// 			'Typs(display-1-strong) Fxs(1) Fxw(w) Pend(8px) M(0) D(i)'
-		// 		)[0].innerHTML; // Change '.name-class' to the actual class
-
-		// 		let shouldSwipeRight = false;
-
-		// 		// Check if language contains Russian or Ukrainian
-		// 		if (
-		// 			languageInfo &&
-		// 			(languageInfo.textContent.includes('Russian') || languageInfo.textContent.includes('Ukrainian'))
-		// 		) {
-		// 			shouldSwipeRight = true;
-		// 		}
-
-		// 		// Check if name is in Cyrillic script
-		// 		if (!shouldSwipeRight && nameInfo) {
-		// 			// Regex to check for Cyrillic script
-		// 			const russianNamePattern = /[\u0400-\u04FF]/;
-		// 			if (russianNamePattern.test(nameInfo.textContent)) {
-		// 				shouldSwipeRight = true;
-		// 			}
-		// 		}
-
-		// 		// Swipe right or left based on the conditions
-		// 		if (shouldSwipeRight) {
-		// 			swipeRight();
-		// 		} else {
-		// 			swipeLeft();
-		// 		}
-
-		// 		mutationInstance.disconnect();
-		// 	}
-		// });
-
-		// observer.observe(document, {
-		// 	childList: true,
-		// 	subtree: true,
-		// });
+		if (shouldSwipeRight) {
+			swipeRight()
+		} else {
+			swipeLeft()
+		}
 	}
 }
 
 // Function to simulate right swipe
 function swipeRight() {
-	const rightSwipeButtonParent = document.getElementsByClassName('Bgi($g-ds-overlay-profile-button-gamepad)');
+	const rightSwipeButtonParent = document.getElementsByClassName(
+		'Bgi($g-ds-overlay-profile-button-gamepad)'
+	)
 	if (rightSwipeButtonParent && rightSwipeButtonParent.length > 0) {
-		const rightSwipeButton = rightSwipeButtonParent[0].querySelector('div > div > div:nth-child(4) > button');
-		rightSwipeButton.click();
-		console.log('Swiped right');
+		const rightSwipeButton = rightSwipeButtonParent[0].querySelector(
+			'div > div > div:nth-child(4) > button'
+		)
+		rightSwipeButton.click()
+		console.log('Swiped right')
 	}
 }
 
 // Function to simulate left swipe
 function swipeLeft() {
-	const leftSwipeButtonParent = document.getElementsByClassName('Bgi($g-ds-overlay-profile-button-gamepad)');
+	const leftSwipeButtonParent = document.getElementsByClassName(
+		'Bgi($g-ds-overlay-profile-button-gamepad)'
+	)
 
 	if (leftSwipeButtonParent && leftSwipeButtonParent.length > 0) {
-		const leftSwipeButton = leftSwipeButtonParent[0].querySelector('div > div > div:nth-child(2) > button');
-		leftSwipeButton.click();
-		console.log('Swiped left');
+		const leftSwipeButton = leftSwipeButtonParent[0].querySelector(
+			'div > div > div:nth-child(2) > button'
+		)
+		leftSwipeButton.click()
+		console.log('Swiped left')
 	}
 }
 
-// Repeatedly check profiles at intervals based on speed setting
-async function startAutoSwiping() {
-	let result = await chrome.storage.local.get(['speed']);
-	speed = result.speed || 750; // Default to 750 ms if not set
-	intervalId = setInterval(checkProfileAndSwipe, speed);
+function startAutoSwiping() {
+	if (shouldContinueSwiping) {
+		intervalId = setInterval(checkProfileAndSwipe, 1000) // Check initially every second
+	}
 }
 
-// Listen for messages from background or popup scripts
+function pauseSwiping() {
+	clearInterval(intervalId)
+	setTimeout(() => {
+		if (shouldContinueSwiping) {
+			startAutoSwiping() // Resume swiping after pause
+		}
+	}, 15 * 60 * 1000) // Pause for 15 minutes
+}
+
+function handleRegularBreaks() {
+	pauseIntervalId = setInterval(pauseSwiping, 60 * 60 * 1000) // Pause every hour
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message.command === 'start') {
-		// console.log('Starting auto swiper...');
-		shouldContinueSwiping = true;
-		startAutoSwiping();
+		shouldContinueSwiping = true
+		startAutoSwiping()
+		handleRegularBreaks()
 	}
 	if (message.command === 'stop') {
-		// console.log('Stopping auto swiper...');
-		shouldContinueSwiping = false;
-		clearInterval(intervalId); // This may need a more robust implementation to clear correctly
+		shouldContinueSwiping = false
+		clearInterval(intervalId)
+		clearInterval(pauseIntervalId)
 	}
-});
+})
 
 // Start swiping when the script loads
-startAutoSwiping();
+startAutoSwiping()
+handleRegularBreaks()
--- popup.js
+++ popup.js
@@ -1,69 +1,69 @@
 chrome.storage.local.get(['speed', 'hours'], function (result) {
-	const speed = result.speed || 750; // Default to 500 ms if not set
-	const hours = result.hours || 1; // Default to 1 hour if not set
-	document.getElementById('speed').setAttribute('value', speed);
-	document.getElementById('hours').setAttribute('value', hours);
-});
+	const speed = result.speed || 1 // Default to 1 s if not set
+	const hours = result.hours || 1 // Default to 1 hour if not set
+	document.getElementById('speed').setAttribute('value', speed)
+	document.getElementById('hours').setAttribute('value', hours)
+})
 
 document.getElementById('start').addEventListener('click', () => {
 	// validate inputs from user
-	let speed = document.getElementById('speed').value;
-	if (parseInt(speed) < 750) {
-		document.getElementById('speedError').textContent = 'Minimun Recommended Speed is 750 ms';
-		return;
-		// speed = 750;
+	let speed = document.getElementById('speed').value
+	if (parseInt(speed) < 1) {
+		document.getElementById('speedError').textContent = 'Minimun Recommended Speed is 1 second'
+		return
+		// speed = 1;
 		// document.getElementById('speed').setAttribute('value', speed);
 	}
-	let hours = document.getElementById('hours').value;
+	let hours = document.getElementById('hours').value
 	if (parseInt(hours) < 1) {
-		hours = 1;
-		document.getElementById('hours').setAttribute('value', hours);
+		hours = 1
+		document.getElementById('hours').setAttribute('value', hours)
 	}
 	////////
 
 	chrome.storage.local.set({ speed, hours }, () => {
-		chrome.alarms.create({ delayInMinutes: parseInt(hours) * 60 });
-		chrome.runtime.sendMessage({ command: 'start' });
-		startCountdown(parseInt(hours));
-	});
-});
+		chrome.alarms.create({ delayInMinutes: parseInt(hours) * 60 })
+		chrome.runtime.sendMessage({ command: 'start' })
+		startCountdown(parseInt(hours))
+	})
+})
 
 document.getElementById('stop').addEventListener('click', () => {
 	chrome.tabs.query({ url: '*://*.tinder.com/*' }, (tabs) => {
 		for (let tab of tabs) {
-			chrome.tabs.sendMessage(tab.id, { command: 'stop' });
+			chrome.tabs.sendMessage(tab.id, { command: 'stop' })
 		}
-		stopCountdown();
-	});
-});
+		stopCountdown()
+	})
+})
 
 chrome.storage.onChanged.addListener((changes, areaName) => {
-	debugger;
-});
+	debugger
+})
 
-let countdownInterval;
+let countdownInterval
 
 function startCountdown(hours) {
-	let endTime = Date.now() + hours * 3600000;
+	let endTime = Date.now() + hours * 3600000
 	countdownInterval = setInterval(function () {
-		let msLeft = endTime - Date.now();
+		let msLeft = endTime - Date.now()
 		if (msLeft <= 0) {
-			clearInterval(countdownInterval);
-			document.getElementById('timer').textContent = 'Time is up!';
-			return;
+			clearInterval(countdownInterval)
+			document.getElementById('timer').textContent = 'Time is up!'
+			return
 		}
-		let hours = Math.floor(msLeft / 3600000);
-		msLeft %= 3600000;
-		let minutes = Math.floor(msLeft / 60000);
-		msLeft %= 60000;
-		let seconds = Math.floor(msLeft / 1000);
+		let hours = Math.floor(msLeft / 3600000)
+		msLeft %= 3600000
+		let minutes = Math.floor(msLeft / 60000)
+		msLeft %= 60000
+		let seconds = Math.floor(msLeft / 1000)
 		document.getElementById(
 			'timer'
-		).textContent = `${hours} hours, ${minutes} minutes, ${seconds} seconds remaining`;
-	}, 1000);
+		).textContent = `${hours} hours, ${minutes} minutes, ${seconds} seconds remaining`
+	}, 1000)
 }
 
 function stopCountdown() {
-	clearInterval(countdownInterval);
-	document.getElementById('timer').textContent = 'Countdown stopped.';
+	clearInterval(countdownInterval)
+	document.getElementById('timer').textContent = 'Countdown stopped.'
 }
